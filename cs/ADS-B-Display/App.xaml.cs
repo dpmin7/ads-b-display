8e8f00da262ca825381f2215eb7eb53cc8dee040
dkdk.kim <dkdk.kim@lge.com>
2025-06-21 17:05:21 -0400

    Add NLog library for logging.

--- cs/ADS-B-Display/App.xaml.cs
+++ cs/ADS-B-Display/App.xaml.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
 using System.Windows;
 
 namespace ADS_B_Display
@@ -13,5 +9,20 @@ namespace ADS_B_Display
     /// </summary>
     public partial class App : Application
     {
+        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            // 애플리케이션 시작 시 필요한 초기화 작업을 여기에 추가할 수 있습니다.
+            // 예: 데이터베이스 연결, 설정 로드 등
+            logger.Info("ADS-B-Display Start.");
+        }
+        protected override void OnExit(ExitEventArgs e)
+        {
+            logger.Info("ADS-B-Display End.");
+            // 애플리케이션 종료 시 필요한 정리 작업을 여기에 추가할 수 있습니다.
+            base.OnExit(e);
+        }
     }
 }
